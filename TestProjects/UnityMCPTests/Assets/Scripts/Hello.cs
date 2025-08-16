ce53639090d5122bafa277988c54170e0ff58283
Marcus Sanatan <msanatan@gmail.com>
2025-08-15 21:24:55 -0400

    Set up Unit tests (#220)
    * Add a base Unity project to run unit tests in
    
    * Add Windsurf IDE support so I can test more easily
    
    * Add a dummy script
    
    * feat: add unit tests for CommandRegistry and document prefab asset workflows
    
    * Run tests when code is pushed to main
    
    * Bump version of actions
    
    * Install the MCP plugin via relative path
    
    * Remove test branch from GH workflow

---
+++ TestProjects/UnityMCPTests/Assets/Scripts/Hello.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+}
---
+++ TestProjects/UnityMCPTests/Assets/Tests/EditMode/CommandRegistryTests.cs
@@ -0,0 +1,30 @@
+using System;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using UnityMcpBridge.Editor.Tools;
+
+namespace UnityMCPTests.EditMode
+{
+    public class CommandRegistryTests
+    {
+        [Test]
+        public void GetHandler_ReturnsNull_ForUnknownCommand()
+        {
+            var unknown = "HandleDoesNotExist";
+            var handler = CommandRegistry.GetHandler(unknown);
+            Assert.IsNull(handler, "Expected null handler for unknown command name.");
+        }
+
+        [Test]
+        public void GetHandler_ReturnsManageGameObjectHandler()
+        {
+            var handler = CommandRegistry.GetHandler("HandleManageGameObject");
+            Assert.IsNotNull(handler, "Expected a handler for HandleManageGameObject.");
+
+            var methodInfo = handler.Method;
+            Assert.AreEqual("HandleCommand", methodInfo.Name, "Handler method name should be HandleCommand.");
+            Assert.AreEqual(typeof(ManageGameObject), methodInfo.DeclaringType, "Handler should be declared on ManageGameObject.");
+            Assert.IsNull(handler.Target, "Handler should be a static method (no target instance).");
+        }
+    }
+}
